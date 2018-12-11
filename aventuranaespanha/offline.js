e65ceac952e401e0647758d6e0882833f17b02b4
wesley <wesley.wmcc@hotmail.com>
2018-12-10 22:48:11 -0300

    zevitupdate
    jogo de ze vitu do tcc vai da certo em nome de jesus

---
+++ aventuranaespanha/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1544492682,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bdireita-sheet0.png",
+		"images/besquerda-sheet0.png",
+		"images/ichao.png",
+		"images/inpc-sheet0.png",
+		"images/ipersonagem-sheet0.png",
+		"images/iitem-sheet0.png",
+		"images/iitem-sheet1.png",
+		"images/iitem-sheet2.png",
+		"images/ilimite-sheet0.png",
+		"images/sensorsala-sheet0.png",
+		"images/icamera-sheet0.png",
+		"images/tflutua.png",
+		"images/ibandeiras-sheet0.png",
+		"images/ilocalizador-sheet0.png",
+		"images/logo-sheet0.png",
+		"images/iplay-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"missoeses.xml",
+		"missoespt.xml"
+	]
+}
